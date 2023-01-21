ab2716b47f0940c24787db7258f9848c9007171e
Shreyoshi Mahato <shreyoshimahato@gmail.com>
2023-01-22 02:19:47 +0530

    test for adding flight as null or valid then check the size of flight list

--- airline/src/main/java/edu/pdx/cs410J/smahato/Airline.java
+++ airline/src/main/java/edu/pdx/cs410J/smahato/Airline.java
@@ -2,30 +2,66 @@ package edu.pdx.cs410J.smahato;
 
 import edu.pdx.cs410J.AbstractAirline;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
+/**
+ * Airline has and name and a list of flights
+ */
 public class Airline extends AbstractAirline<Flight> {
+  /**
+   * Name of the airline
+   */
   private final String name;
+  /**
+   * List of flights
+   */
+  private List<Flight> flights;
 
+  /**
+   * Creates a new airline with the given name and empty list of flights
+   *
+   * @param name Name of the airline
+   */
   public Airline(String name) {
     if (name == null || name.isBlank()) {
       throw new NullPointerException("Airline name cannot be null or blank");
     }
     this.name = name;
+    this.flights = new ArrayList<>();
   }
 
+  /**
+   * Returns the name of the airline
+   *
+   * @return Name of the airline
+   */
   @Override
   public String getName() {
     return this.name;
   }
 
+  /**
+   * Adds a flight to the list of flights
+   *
+   * @param flight Flight to be added
+   */
   @Override
   public void addFlight(Flight flight) {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    if (flight == null) {
+      throw new NullPointerException("Flight cannot be null");
+    }
+    flights.add(flight);
   }
 
+  /**
+   * Returns the list of flights
+   *
+   * @return List of flights
+   */
   @Override
   public Collection<Flight> getFlights() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    return this.flights;
   }
 }
--- airline/src/test/java/edu/pdx/cs410J/smahato/AirlineTest.java
+++ airline/src/test/java/edu/pdx/cs410J/smahato/AirlineTest.java
@@ -2,9 +2,7 @@ package edu.pdx.cs410J.smahato;
 
 import org.junit.jupiter.api.Test;
 
-import java.time.DateTimeException;
-
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -29,6 +27,34 @@ public class AirlineTest {
   void testValidName() {
     Airline airline = new Airline("CS410J Air Express");
     assertThat(airline.getName(), equalTo("CS410J Air Express"));
+  }
+
+  // Test4 : create an airline with a valid name as CS410J Air Express and add a flight
+  @Test
+  void testAddFlight() {
+    Airline airline = new Airline("CS410J Air Express");
+    Flight flight = new Flight(1, "PDX", "LAX", "10/10/2020 10:10", "10/10/2020 11:10");
+    airline.addFlight(flight);
+    assertThat(airline.getFlights().size(), equalTo(1));
+  }
+
+  // Test5 : create an airline with a valid name as CS410J Air Express and two flights
+  @Test
+  void testAddTwoFlights() {
+    Airline airline = new Airline("CS410J Air Express");
+    Flight flight1 = new Flight(1, "PDX", "LAX", "10/10/2020 10:10", "10/10/2020 11:10");
+    Flight flight2 = new Flight(2, "LAX", "PDX", "10/10/2020 12:10", "10/10/2020 13:10");
+    airline.addFlight(flight1);
+    airline.addFlight(flight2);
+    assertThat(airline.getFlights().size(), equalTo(2));
+  }
+
+  // Test6 : create an airline with a valid name as CS410J Air Express and add a flight with null flight
+  // NullPointerException is thrown because flight cannot be null
+  @Test
+  void testAddNullFlight() {
+    Airline airline = new Airline("CS410J Air Express");
+    assertThrows(NullPointerException.class, () -> airline.addFlight(null));
   }
 
 }
