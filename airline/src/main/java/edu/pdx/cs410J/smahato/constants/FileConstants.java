749e194383bc86a5136da21640ba175fba0966c7
Shreyoshi Mahato <shreyoshimahato@gmail.com>
2023-02-08 08:49:58 +0530

    added fileconstants documentation

--- airline/src/main/java/edu/pdx/cs410J/smahato/constants/FileConstants.java
+++ airline/src/main/java/edu/pdx/cs410J/smahato/constants/FileConstants.java
@@ -1,7 +1,16 @@
 package edu.pdx.cs410J.smahato.constants;
 
+/**
+ * contains constants like separators used in file
+ */
 public interface FileConstants {
 
+  /**
+   * separator used for separating fields in text file
+   */
   String TEXT_FILE_SEPARATOR = "#";
+  /**
+   * separator used for separating fields in pretty file
+   */
   String PRETTY_FILE_SEPARATOR = " | ";
 }
