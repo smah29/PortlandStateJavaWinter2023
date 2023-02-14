e7c3e8c3abe145e5c7b1e2d4f6c031f2d848ff3a
SHREYOSHI MAHATO <shreyoshimahato@gmail.com>
2023-02-15 03:38:53 +0530

    AboutEnums.java koans done

--- koans/src/beginner/AboutEnums.java
+++ koans/src/beginner/AboutEnums.java
@@ -16,17 +16,16 @@ public class AboutEnums {
     @Koan
     public void basicEnums() {
         Colors blue = Colors.Blue;
-        assertEquals(blue == Colors.Blue, __);
-        assertEquals(blue == Colors.Red, __);
-        assertEquals(blue instanceof Colors, __);
+        assertEquals(blue == Colors.Blue, true);
+        assertEquals(blue == Colors.Red, false);
+        assertEquals(blue instanceof Colors, true);
     }
 
     @Koan
     public void basicEnumsAccess() {
         Colors[] colorArray = Colors.values();
-        assertEquals(colorArray[2], __);
+        assertEquals(colorArray[2], Colors.Green);
     }
-
     enum SkatSuits {
         Clubs(12), Spades(11), Hearts(10), Diamonds(9);
 
@@ -41,8 +40,8 @@ public class AboutEnums {
     public void enumsWithAttributes() {
         // value is private but we still can access it. Why?
         // Try moving the enum outside the AboutEnum class... What do you expect?
-        // What happens?
-        assertEquals(SkatSuits.Clubs.value > SkatSuits.Spades.value, __);
+        // What happens? value will not be accessible anymore.
+        assertEquals(SkatSuits.Clubs.value > SkatSuits.Spades.value, true);
     }
 
     enum OpticalMedia {
@@ -61,7 +60,7 @@ public class AboutEnums {
 
     @Koan
     public void enumsWithMethods() {
-        assertEquals(OpticalMedia.CD.getCoolnessFactor(), __);
-        assertEquals(OpticalMedia.BluRay.getCoolnessFactor(), __);
+        assertEquals(OpticalMedia.CD.getCoolnessFactor(), -3500);
+        assertEquals(OpticalMedia.BluRay.getCoolnessFactor(), 490000);
     }
 }
