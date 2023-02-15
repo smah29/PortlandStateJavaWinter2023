a1ef2fa8d868ce9e7b8c56936942efc61bf67c96
SHREYOSHI MAHATO <shreyoshimahato@gmail.com>
2023-02-15 08:30:43 +0530

    AboutLocale.java koans done

--- koans/src/intermediate/AboutLocale.java
+++ koans/src/intermediate/AboutLocale.java
@@ -19,24 +19,24 @@ public class AboutLocale {
         Date date = cal.getTime();
         Locale localeBR = new Locale("pt", "BR"); // portuguese, Brazil
         DateFormat dateformatBR = DateFormat.getDateInstance(DateFormat.FULL, localeBR);
-        assertEquals(dateformatBR.format(date), __);
+        assertEquals(dateformatBR.format(date), "domingo, 3 de abril de 2011");
 
         Locale localeJA = new Locale("de"); // German
         DateFormat dateformatJA = DateFormat.getDateInstance(DateFormat.FULL, localeJA);
         // Well if you don't know how to type these characters, try "de", "it" or "us" ;-)
-        assertEquals(dateformatJA.format(date), __);
+        assertEquals(dateformatJA.format(date), "Sonntag, 3. April 2011");
     }
 
     @Koan
     public void getCountryInformation() {
         Locale locBR = new Locale("pt", "BR");
-        assertEquals(locBR.getDisplayCountry(), __);
-        assertEquals(locBR.getDisplayCountry(locBR), __);
+        assertEquals(locBR.getDisplayCountry(), "Brazil");
+        assertEquals(locBR.getDisplayCountry(locBR), "Brasil");
 
         Locale locCH = new Locale("it", "CH");
-        assertEquals(locCH.getDisplayCountry(), __);
-        assertEquals(locCH.getDisplayCountry(locCH), __);
-        assertEquals(locCH.getDisplayCountry(new Locale("de", "CH")), __);
+        assertEquals(locCH.getDisplayCountry(), "Switzerland");
+        assertEquals(locCH.getDisplayCountry(locCH), "Svizzera");
+        assertEquals(locCH.getDisplayCountry(new Locale("de", "CH")), "Schweiz");
     }
 
 }
