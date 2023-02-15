de8d9f873fb81f8394ac1f09aeb40e6dcca3cb3d
SHREYOSHI MAHATO <shreyoshimahato@gmail.com>
2023-02-15 06:58:20 +0530

    AboutInnerClasses.java koans done

--- koans/src/intermediate/AboutInnerClasses.java
+++ koans/src/intermediate/AboutInnerClasses.java
@@ -24,13 +24,13 @@ public class AboutInnerClasses {
     @Koan
     public void creatingInnerClassInstance() {
         Inner someObject = new Inner();
-        assertEquals(someObject.doStuff(), __);
+        assertEquals(someObject.doStuff(), "stuff");
     }
 
     @Koan
     public void creatingInnerClassInstanceWithOtherSyntax() {
         AboutInnerClasses.Inner someObject = this.new Inner();
-        assertEquals(someObject.doStuff(), __);
+        assertEquals(someObject.doStuff(), "stuff");
     }
 
     private int x = 10;
@@ -38,7 +38,7 @@ public class AboutInnerClasses {
     @Koan
     public void accessingOuterClassMembers() {
         Inner someObject = new Inner();
-        assertEquals(someObject.returnOuter(), __);
+        assertEquals(someObject.returnOuter(), 10);
     }
 
     @Koan
@@ -48,7 +48,7 @@ public class AboutInnerClasses {
                 return 100;
             }
         }
-        assertEquals(new MethodInnerClass().oneHundred(), __);
+        assertEquals(new MethodInnerClass().oneHundred(), 100);
         // Where can you use this class?
     }
 
@@ -71,9 +71,9 @@ public class AboutInnerClasses {
     @Koan
     public void innerClassesInMethodsThatEscape() {
         AnotherInnerClass ic = new AnotherInnerClass();
-        assertEquals(ic.thousand(), __);
+        assertEquals(ic.thousand(), 1000);
         AnotherInnerClass theCrazyIC = ic.crazyReturn();
-        assertEquals(theCrazyIC.thousand(), __);
+        assertEquals(theCrazyIC.thousand(), 2000);
     }
 
     int theAnswer() {
@@ -87,14 +87,15 @@ public class AboutInnerClasses {
                 return 23;
             }
         };// <- Why do you need a semicolon here?
-        assertEquals(anonymous.theAnswer(), __);
+        // The semicolon is required to mark the end of the statement. Defining and initializing in the same statement.
+        assertEquals(anonymous.theAnswer(), 23);
     }
 
     @Koan
     public void creatingAnonymousInnerClassesToImplementInterface() {
         Ignoreable ignoreable = new Ignoreable() {
             public String ignoreAll() {
-                return null;
+                return "SomeInterestingString";
             }
         }; // Complete the code so that the statement below is correct.
         // Look at the koan above for inspiration
@@ -102,6 +103,7 @@ public class AboutInnerClasses {
         // Did you just created an object of an interface type?
         // Or did you create a class that implemented this interface and
         // an object of that type?
+        // created an anonymous inner class that implements the interface
     }
 
     @Koan
@@ -111,7 +113,8 @@ public class AboutInnerClasses {
         // Try to change the 'Inner' below to "AboutInnerClasses'
         // Why do you get an error?
         // What does that imply for inner classes and inheritance?
-        assertEquals(someObject instanceof Inner, __);
+        // Outer classes are not inherited by the Inner class
+        assertEquals(someObject instanceof Inner, true);
     }
 
     class OtherInner extends AboutInnerClasses {
@@ -122,7 +125,7 @@ public class AboutInnerClasses {
         OtherInner someObject = new OtherInner();
         // What do you expect here?
         // Compare this result with the last koan. What does that mean?
-        assertEquals(someObject instanceof AboutInnerClasses, __);
+        assertEquals(someObject instanceof AboutInnerClasses, true);
     }
 
     static class StaticInnerClass {
@@ -134,17 +137,19 @@ public class AboutInnerClasses {
     @Koan
     public void staticInnerClass() {
         StaticInnerClass someObject = new StaticInnerClass();
-        assertEquals(someObject.importantNumber(), __);
+        assertEquals(someObject.importantNumber(), 3);
         // What happens if you try to access 'x' or 'theAnswer' from the outer class?
         // What does this mean for static inner classes?
         // Try to create a sub package of this package which is named 'StaticInnerClass'
         // Does it work? Why not?
+        // static inner class is not associated with any instance of the outer class, so it has no access to the instance variables of the outer class,
+        // but can access static members of the outer class
     }
 
     @Koan
     public void staticInnerClassFullyQualified() {
         AboutInnerClasses.StaticInnerClass someObject = new AboutInnerClasses.StaticInnerClass();
-        assertEquals(someObject.importantNumber(), __);
+        assertEquals(someObject.importantNumber(), 3);
     }
 
 }
