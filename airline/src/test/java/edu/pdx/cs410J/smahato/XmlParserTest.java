f984fd336d30b8c5b2857bff70056fa75f49de3a
SHREYOSHI MAHATO <shreyoshimahato@gmail.com>
2023-02-21 13:22:38 +0530

    some documentation in XmlParser

--- airline/src/test/java/edu/pdx/cs410J/smahato/XmlParserTest.java
+++ airline/src/test/java/edu/pdx/cs410J/smahato/XmlParserTest.java
@@ -12,8 +12,16 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+/**
+ * Unit tests for the {@link XmlParser} class.
+ */
 public class XmlParserTest {
 
+  /**
+   * Tests that a valid XML file can be parsed.
+   *
+   * @throws ParserException if the XML file cannot be parsed
+   */
   @Test
   void validXmlFileCanBeParsed() throws ParserException {
     InputStream resource = getClass().getResourceAsStream("valid-airline.xml");
@@ -24,6 +32,11 @@ public class XmlParserTest {
     assertThat(airline.getName(), equalTo("Valid Airlines"));
   }
 
+  /**
+   * Tests that an invalid XML file throws an exception.
+   *
+   * @throws ParserException if the XML file cannot be parsed
+   */
   @Test
   void invalidTextFile() throws ParserException {
     InputStream resource = getClass().getResourceAsStream("valid-airline.txt");
@@ -33,6 +46,11 @@ public class XmlParserTest {
     assertThrows(XMLException.class, parser::parse);
   }
 
+  /**
+   * Tests that an invalid day in XML file throws an exception.
+   *
+   * @throws ParserException if the XML file cannot be parsed
+   */
   @Test
   void invalidDay() throws ParserException {
     InputStream resource = getClass().getResourceAsStream("invalid-airline-day.xml");
@@ -42,6 +60,11 @@ public class XmlParserTest {
     assertThrows(XMLException.class, parser::parse);
   }
 
+  /**
+   * Tests that an invalid month in XML file throws an exception.
+   *
+   * @throws ParserException if the XML file cannot be parsed
+   */
   @Test
   void invalidMonth() throws ParserException {
     InputStream resource = getClass().getResourceAsStream("invalid-airline-month.xml");
@@ -51,6 +74,11 @@ public class XmlParserTest {
     assertThrows(XMLException.class, parser::parse);
   }
 
+  /**
+   * Tests that an invalid year in XML file throws an exception.
+   *
+   * @throws ParserException if the XML file cannot be parsed
+   */
   @Test
   void invalidYear() throws ParserException {
     InputStream resource = getClass().getResourceAsStream("invalid-airline-year.xml");
@@ -60,6 +88,11 @@ public class XmlParserTest {
     assertThrows(XMLException.class, parser::parse);
   }
 
+  /**
+   * Tests that an invalid hour in XML file throws an exception.
+   *
+   * @throws ParserException if the XML file cannot be parsed
+   */
   @Test
   void invalidHour() throws ParserException {
     InputStream resource = getClass().getResourceAsStream("invalid-airline-hour.xml");
@@ -69,6 +102,11 @@ public class XmlParserTest {
     assertThrows(XMLException.class, parser::parse);
   }
 
+  /**
+   * Tests that an invalid minute in XML file throws an exception.
+   *
+   * @throws ParserException if the XML file cannot be parsed
+   */
   @Test
   void invalidMinute() throws ParserException {
     InputStream resource = getClass().getResourceAsStream("invalid-airline-minute.xml");
@@ -78,6 +116,11 @@ public class XmlParserTest {
     assertThrows(XMLException.class, parser::parse);
   }
 
+  /**
+   * Tests that an invalid flight number XML file throws an exception.
+   *
+   * @throws ParserException if the XML file cannot be parsed
+   */
   @Test
   void invalidFlightNumber() throws ParserException {
     InputStream resource = getClass().getResourceAsStream("invalid-airline-flightNum.xml");
@@ -87,6 +130,11 @@ public class XmlParserTest {
     assertThrows(XMLException.class, parser::parse);
   }
 
+  /**
+   * Tests that an invalid airport code in XML file throws an exception.
+   *
+   * @throws ParserException if the XML file cannot be parsed
+   */
   @Test
   void invalidAirportCode() throws ParserException {
     InputStream resource = getClass().getResourceAsStream("invalid-airline-airport.xml");
@@ -96,6 +144,11 @@ public class XmlParserTest {
     assertThrows(XMLException.class, parser::parse);
   }
 
+  /**
+   * Tests that an invalid element in XML file throws an exception.
+   *
+   * @throws ParserException if the XML file cannot be parsed
+   */
   @Test
   void invalidRootElement() throws ParserException {
     InputStream resource = getClass().getResourceAsStream("invalid-airline-root.xml");
