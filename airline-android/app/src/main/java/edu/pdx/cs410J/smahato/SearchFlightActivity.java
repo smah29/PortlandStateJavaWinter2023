d0952edfcc399297aa45262fc1e974e871e35d64
SHREYOSHI MAHATO <shreyoshimahato@gmail.com>
2023-03-19 11:22:40 +0530

    show pretty flights screen done

--- airline-android/app/src/main/java/edu/pdx/cs410J/smahato/SearchFlightActivity.java
+++ airline-android/app/src/main/java/edu/pdx/cs410J/smahato/SearchFlightActivity.java
@@ -2,6 +2,8 @@ package edu.pdx.cs410J.smahato;
 
 import static edu.pdx.cs410J.smahato.constants.AirlineConstants.DESTINATION;
 import static edu.pdx.cs410J.smahato.constants.AirlineConstants.SOURCE;
+import static edu.pdx.cs410J.smahato.constants.Messages.airlineDoesNotExist;
+import static edu.pdx.cs410J.smahato.constants.Messages.flightDoesNotExist;
 import static edu.pdx.cs410J.smahato.utils.ActivityHelper.isEmpty;
 import static edu.pdx.cs410J.smahato.utils.ActivityHelper.isInputLengthZero;
 import static edu.pdx.cs410J.smahato.utils.ActivityHelper.validateAirportCode;
@@ -10,6 +12,7 @@ import static edu.pdx.cs410J.smahato.utils.ActivityHelper.validateArrival;
 import static edu.pdx.cs410J.smahato.utils.ActivityHelper.validateDeparture;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -17,10 +20,19 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
+import edu.pdx.cs410J.ParserException;
+import edu.pdx.cs410J.smahato.storage.TextParser;
+import edu.pdx.cs410J.smahato.utils.Airline;
 import edu.pdx.cs410J.smahato.utils.OnTextChangeWatcher;
 
 public class SearchFlightActivity extends AppCompatActivity {
@@ -84,12 +96,42 @@ public class SearchFlightActivity extends AppCompatActivity {
         EditText airlineName = findViewById(R.id.airlineNameValue);
         EditText source = findViewById(R.id.sourceValue);
         EditText destination = findViewById(R.id.destinationVal);
-        String str = airlineName.getText() + " " + source.getText() + " "
-                + destination.getText();
-        //Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+        TextView errorMessage = findViewById(R.id.errorMessage);
 
-        Intent intent = new Intent(this, ShowFlightsActivity.class);
-        intent.putExtra("flight1", str);
-        startActivity(intent);
+        String airlineNameText = airlineName.getText().toString();
+        String sourceValue = source.getText().toString();
+        String destinationValue = destination.getText().toString();
+        File file = new File(this.getDataDir(), airlineNameText);
+        if (file.exists()) {
+            try {
+                Airline airline = new TextParser(new FileReader(file)).parse();
+                if (airline == null) {
+                    errorMessage.setText(airlineDoesNotExist(airlineNameText));
+                    errorMessage.setError("");
+                } else {
+                    Airline newAirline = new Airline(airlineNameText);
+                    if (source.length() > 0 && destination.length() > 0) {
+                        List<Flight> flights = airline.getFlights(sourceValue.toUpperCase(), destinationValue.toUpperCase());
+                        if (flights.isEmpty()) {
+                            errorMessage.setText(flightDoesNotExist(sourceValue, destinationValue, airlineNameText));
+                            errorMessage.setTextColor(Color.RED);
+                            return;
+                        } else
+                            newAirline.getFlights().addAll(flights);
+                    } else {
+                        newAirline = airline;
+                    }
+                    Intent intent = new Intent(this, ShowFlightsActivity.class);
+                    intent.putExtra(getString(R.string.airline), newAirline);
+                    startActivity(intent);
+                }
+            } catch (ParserException | FileNotFoundException e) {
+                errorMessage.setText(airlineDoesNotExist(airlineNameText));
+                errorMessage.setTextColor(Color.RED);
+            }
+        } else {
+            errorMessage.setText(airlineDoesNotExist(airlineNameText));
+            errorMessage.setTextColor(Color.RED);
+        }
     }
 }
--- airline-android/app/src/main/java/edu/pdx/cs410J/smahato/ShowFlightsActivity.java
+++ airline-android/app/src/main/java/edu/pdx/cs410J/smahato/ShowFlightsActivity.java
@@ -4,10 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ScrollView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+import edu.pdx.cs410J.AirportNames;
+import edu.pdx.cs410J.smahato.utils.Airline;
 
 public class ShowFlightsActivity extends AppCompatActivity {
 
@@ -16,9 +25,33 @@ public class ShowFlightsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.show_flights);
         Intent intent = getIntent();
-        String airline = intent.getStringExtra("flight1");
-        ListView listView = (ListView) findViewById(R.id.prettyFlights);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Arrays.asList(airline, airline));
+        Airline airline = intent.getSerializableExtra(getString(R.string.airline), Airline.class);
+        ListView listView = findViewById(R.id.prettyFlights);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, prettyPrint(airline));
         listView.setAdapter(arrayAdapter);
+    }
+
+    private List<String> prettyPrint(Airline airline) {
+        List<String> list = new ArrayList<>();
+        list.add("Flights for airline " + airline.getName());
+        AtomicInteger count = new AtomicInteger();
+
+        new ArrayList<>(airline.getFlights()).stream().sorted().forEach(flight -> {
+
+            long durationInMinutes = Duration.between(flight.getDeparture().toInstant(),
+                    flight.getArrival().toInstant()).toMinutes();
+
+            String[] flightDetails = {
+                    "# : " + count.incrementAndGet(),
+                    "Flight : " + flight.getNumber(),
+                    "Source : " + AirportNames.getName(flight.getSource()),
+                    "Destination : " + AirportNames.getName(flight.getDestination()),
+                    "Departure : " + flight.getDepartureString(),
+                    "Arrival : " + flight.getArrivalString(),
+                    "Duration(minutes) : " + durationInMinutes};
+
+            list.add(Arrays.stream(flightDetails).collect(Collectors.joining("\n")));
+        });
+        return list;
     }
 }
